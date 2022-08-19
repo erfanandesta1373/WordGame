5a64e661d37c09773a9281bbc74f4c5715e6186c
Erfan Andesta <erfan.andesta@mindvalley.com>
2022-08-19 19:10:06 +0430

    Initial Commit

---
+++ WordGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordGame
+//
+//  Created by Erfan Andesta on 5/28/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
