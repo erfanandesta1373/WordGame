bf89a164520a3844f6ca842cbeec324ef6d98591
Erfan Andesta <erfan.andesta@mindvalley.com>
2022-08-20 19:57:37 +0430

    [FEAT] Add timer + Exit functionality (MILESTONE 2)

--- WordGame/Modules/Game/ViewModel/WordGameViewModel.swift
+++ WordGame/Modules/Game/ViewModel/WordGameViewModel.swift
@@ -14,6 +14,7 @@ class WordGameViewModel {
     private let jsonLoader: JSONLoader
     private var correctWords = Set<WordPair>()
     private var cancellables = Set<AnyCancellable>()
+    private weak var timer: Timer?
     let currentPair = CurrentValueSubject<WordPair?, Never>(nil)
     let guess = PassthroughSubject<Bool, Never>()
     let points = CurrentValueSubject<(wrong: Int, correct: Int), Never>((0, 0))
@@ -23,6 +24,7 @@ class WordGameViewModel {
         self.jsonLoader = jsonLoader
         correctWords = loadWords()
         makeGame()
+        
         
         guess
             .sink { isCorrect in
@@ -35,18 +37,33 @@ class WordGameViewModel {
                 }
                 self.makeGame()
             }.store(in: &cancellables)
+        
+        points
+            .sink { wrong, correct in
+                let sum = wrong + correct
+                if wrong == 3 || sum == 15 {
+                    exit(0)
+                }
+            }.store(in: &cancellables)
     }
     
     //MARK: - Methods -
     private func loadWords() -> Set<WordPair> {
         return jsonLoader.load(file: "words", for: Self.self, model: Set<WordPair>.self) ?? []
     }
-    
+    private func makeTimer() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
+            self.makeGame()
+            self.points.send((wrong: self.points.value.wrong + 1, correct: self.points.value.correct))
+        }
+        timer?.tolerance = 0.2
+    }
     private func makeGame() {
         let random = Int.random(in: 1...4)
         currentPair.send(makePair(isCorrect: random == 1))
+        makeTimer()
     }
-    
     private func makePair(isCorrect: Bool) -> WordPair? {
         guard let pair = correctWords.randomElement() else { return nil }
         if isCorrect {
